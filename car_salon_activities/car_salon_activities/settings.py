33a8b64497825097d5ed56b41c518495dcfd00bf
Twelve <kana.suzucki@gmail.com>
2023-10-03 14:44:43 +0300

    init

--- car_salon_activities/car_salon_activities/settings.py
+++ car_salon_activities/car_salon_activities/settings.py
@@ -30,42 +30,16 @@ DEFAULT_CHARSET = 'utf8'
 # -------------------------- INSTALLED APPS -----------------------------------
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+
 ]
 
 # -------------------------- MIDDLEWARES --------------------------------------
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-# --------------------------- TEMPLATES ---------------------------------------
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
 ]
 
 # ---------------------------- DATABASES --------------------------------------
@@ -91,23 +65,6 @@ DATABASES = {
     },
 }
 
-# ----------------------------PASSWORD VALIDATION -----------------------------
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
 # ------------------------- LANGUAGE SETTINGS ---------------------------------
 
 LANGUAGE_CODE = 'en-us'
@@ -121,8 +78,6 @@ TIME_ZONE = 'UTC'
 USE_TZ = True
 
 # -------------------------- OTHER SETTINGS -----------------------------------
-
-STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
