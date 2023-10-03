01530b58c9b11e91e45f0f0dc2def85d1e2f9e86
Twelve <kana.suzucki@gmail.com>
2023-10-03 18:01:16 +0300

    init

--- car_salon_activities/car_salon_activities/asgi.py
+++ car_salon_activities/car_salon_activities/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'car_salon_activities.settings')
 
 application = get_asgi_application()
--- car_salon_activities/car_salon_activities/settings.py
+++ car_salon_activities/car_salon_activities/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+
 # -------------------------- MAIN SETTINGS ------------------------------------
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,23 +24,49 @@ DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
-ROOT_URLCONF = 'car_salon_activities.urls'
-
 DEFAULT_CHARSET = 'utf8'
 
 # -------------------------- INSTALLED APPS -----------------------------------
 
 INSTALLED_APPS = [
-
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'user',
+    'ajwt',
+    'salon',
 ]
 
 # -------------------------- MIDDLEWARES --------------------------------------
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# --------------------------- TEMPLATES ---------------------------------------
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
 # ---------------------------- DATABASES --------------------------------------
@@ -79,6 +106,21 @@ USE_TZ = True
 
 # -------------------------- OTHER SETTINGS -----------------------------------
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': ('django.contrib.auth.password_validation.' 'UserAttributeSimilarityValidator'),
+    },
+    {
+        'NAME': ('django.contrib.auth.password_validation.' 'MinimumLengthValidator'),
+    },
+    {
+        'NAME': ('django.contrib.auth.password_validation.' 'CommonPasswordValidator'),
+    },
+    {
+        'NAME': ('django.contrib.auth.password_validation.' 'NumericPasswordValidator'),
+    },
+]
 
 WSGI_APPLICATION = 'car_salon_activities.wsgi.application'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
