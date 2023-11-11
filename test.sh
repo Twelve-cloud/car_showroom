aa674646d42129e286705407d62acf30fe0a5b6a
Twelve <kana.suzucki@gmail.com>
2023-11-11 18:17:38 +0300

    fix: some fixes in github actions ci

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 trap 'cleanup ; printf "${RED}Tests Failed For Unexpected Reasons${NC}\n"' HUP INT QUIT PIPE TERM
-docker compose -p tests ${COMPOSE_TESTS_ENV} ${COMPOSE_TESTS} build && docker compose -p tests ${COMPOSE_TESTS_ENV} ${COMPOSE_TESTS} up -d
+sudo docker compose -p tests ${COMPOSE_TESTS_ENV} ${COMPOSE_TESTS} build && sudo docker compose -p tests ${COMPOSE_TESTS_ENV} ${COMPOSE_TESTS} up -d
 if [ $? -ne 0 ] ; then
 	printf "${RED}Docker Compose Failed${NC}\n"
 	exit -1
@@ -14,5 +14,5 @@ if [ -z ${TEST_EXIT_CODE+x} ] || [ "$TEST_EXIT_CODE" -ne 0 ] ; then
 else
 	printf "${GREEN}Tests Passed${NC}\n"
 fi
-docker compose -p tests ${COMPOSE_TESTS_ENV} ${COMPOSE_TESTS} down
+sudo docker compose -p tests ${COMPOSE_TESTS_ENV} ${COMPOSE_TESTS} down
 exit $TEST_EXIT_CODE
